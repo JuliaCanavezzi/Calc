48d7ba553d5cef05b61a48282d933b38c4bee835
JuliaCanavezzi <jucanavezzi03@gmail.com>
2024-03-04 18:24:55 -0300

    Creation Component

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
+  num1: number = 0;
+  num2: number = 0;
+  result: number = 0;
 
+  calc(){
+    this.result = this.num1 + this.num2;
+  }
+  div(){
+    this.result = this.num1 / this.num2;
+  }
+  sub(){
+    this.result = this.num1 - this.num2;
+  }
+  mult(){
+    this.result = this.num1 * this.num2;
+  }
 }
