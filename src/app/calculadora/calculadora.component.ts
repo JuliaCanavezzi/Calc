5b233e2a25b76f18745ca9a3518bcb96851ce85c
JuliaCanavezzi <jucanavezzi03@gmail.com>
2024-03-04 19:29:16 -0300

    creation buttons

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,25 +1,43 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
-  num1: number = 0;
-  num2: number = 0;
-  result: number = 0;
+  num1: number=0;
+  num2: number=0;
+  soma: number=0;
+  subtracao: number=0;
+  multiplicacao: number=0;
+  divisao: number=0;
 
   calc(){
-    this.result = this.num1 + this.num2;
-  }
-  div(){
-    this.result = this.num1 / this.num2;
+    this.soma = this.num1 + this.num2;
   }
   sub(){
-    this.result = this.num1 - this.num2;
+    this.subtracao = this.num1 - this.num2;
   }
   mult(){
-    this.result = this.num1 * this.num2;
+    this.multiplicacao = this.num1 * this.num2;
+  }
+  div(){
+    if(this.num2 == 0){
+      alert("Não é possível DIVIDIR por ZERO!");
+    }
+    else{
+      this.divisao=this.num1/this.num2;
+    }
+  }
+
+  limpar(){
+    this.num1 = 0;
+    this.num2 = 0;
+    this.soma = 0;
+    this.subtracao = 0;
+    this.multiplicacao = 0;
+    this.divisao = 0;
   }
 }
